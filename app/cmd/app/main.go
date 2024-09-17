7544ce4708439d84b753420ce75675c399376686
@SHMELEV183_EXT <shmelev.mv@mmk.ru>
2024-09-17 14:52:46 +0500

    fix

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -14,4 +15,5 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
